dae03348c52647f73a56e1beff2ada68775a7f78
issamjd <88335660+issamjd@users.noreply.github.com>
2023-08-22 14:49:03 +0300

    add main functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    $("#submitBtn").click(function(){
+      
+        let password = $("#password").val();
+        let confirmation = $("#confirmPassword").val();
+
+        if (password != confirmation){
+            $("#submResult").text("* Passwords do not match")
+            event.preventDefault();
+            return false;
+        }
+
+        // If validation passes, you can proceed with form submission
+        // You might also want to submit the form to the server here
+
+    })
+
+    $("#toggle-mode").click(function(){
+        $("body").toggleClass("dark-mode");
+    })
+
+})
